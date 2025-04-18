4c8075ae0e51fc5c2b898a5fd89c6a7b92902363
Wiroj Sanyod <wiroj.san@ext-axonstech.com>
2025-04-18 16:00:18 +0700

    feat: add assignment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  devIndicators: false,
+  trailingSlash: true,
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,105 +11,159 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+interface Data {
+  type: "Vegetable" | "Fruit";
+  name: string;
+}
+
 export default function Home() {
+  const rawData: Data[] = [
+    {
+      type: "Fruit",
+      name: "Apple",
+    },
+    {
+      type: "Vegetable",
+      name: "Broccoli",
+    },
+    {
+      type: "Vegetable",
+      name: "Mushroom",
+    },
+    {
+      type: "Fruit",
+      name: "Banana",
+    },
+    {
+      type: "Vegetable",
+      name: "Tomato",
+    },
+    {
+      type: "Fruit",
+      name: "Orange",
+    },
+    {
+      type: "Fruit",
+      name: "Mango",
+    },
+    {
+      type: "Fruit",
+      name: "Pineapple",
+    },
+    {
+      type: "Vegetable",
+      name: "Cucumber",
+    },
+    {
+      type: "Fruit",
+      name: "Watermelon",
+    },
+    {
+      type: "Vegetable",
+      name: "Carrot",
+    },
+  ];
+
+  const [dataList, setDataList] = useState<Data[]>(rawData);
+  const [fruitList, setFruitList] = useState<Data[]>([]);
+  const [vegetableList, setVegetableList] = useState<Data[]>([]);
+
+  const setDataByType = (data: Data, lIndex: number) => {
+    if (data.type === "Fruit") {
+      setFruitList((prevState) => [...prevState, data]);
+    } else {
+      setVegetableList((prevState) => [...prevState, data]);
+    }
+
+    setDataList(dataList.filter((_, index) => index != lIndex));
+  };
+
+  const setDataToList = (data: Data, index: number) => {
+    setDataList((prevState) => [...prevState, data]);
+    if (data.type === "Fruit") {
+      setFruitList(fruitList.filter((_, fIndex) => fIndex != index));
+    } else {
+      setVegetableList(vegetableList.filter((_, vIndex) => vIndex != index));
+    }
+  };
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.className} ${geistMono.className} font-[family-name:var(--font-geist-sans)] min-h-screen flex items-center justify-center p-4`}
     >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="bg-white rounded shadow-lg flex w-full max-w-5xl p-6 gap-4 min-h-[720px]">
+        <div className="flex flex-col space-y-2 w-1/4">
+          {dataList.map((data, index) => (
+            <button
+              key={index}
+              className="border px-4 py-2 rounded hover:bg-gray-100"
+              onClick={() => setDataByType(data, index)}
+            >
+              {data.name}
+            </button>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex flex-1 space-x-4">
+          <div
+            className="flex-1 border rounded p-2"
+            onClick={() => {
+              if (fruitList.length > 0) {
+                setDataToList(
+                  fruitList[fruitList.length - 1],
+                  fruitList.length - 1
+                );
+              }
+            }}
+          >
+            <h2 className="font-semibold border-b pb-2 mb-2 text-center">
+              Fruit
+            </h2>
+            <div className="flex flex-col space-y-2 w-full">
+              {fruitList.map((fruit, index) => (
+                <button
+                  key={index}
+                  className="border px-4 py-2 rounded hover:bg-gray-100"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setDataToList(fruit, index);
+                  }}
+                >
+                  {fruit.name}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div
+            className="flex-1 border rounded p-2"
+            onClick={() => {
+              if (vegetableList.length > 0) {
+                setDataToList(
+                  vegetableList[vegetableList.length - 1],
+                  vegetableList.length - 1
+                );
+              }
+            }}
+          >
+            <h2 className="font-semibold border-b pb-2 mb-2 text-center">
+              Vegetable
+            </h2>
+            <div className="flex flex-col space-y-2 w-full">
+              {vegetableList.map((vegetable, index) => (
+                <button
+                  key={index}
+                  className="border px-4 py-2 rounded hover:bg-gray-100"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setDataToList(vegetable, index);
+                  }}
+                >
+                  {vegetable.name}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
